ec571cc5ae90ee47aeccb1d03db35aa8decce46b
AngelikaMilchenko <angelika.mila55@gmail.com>
2022-10-02 13:21:30 +0500

    В файл LOCATORS добавлены локаторы keyboard_button, keyboard_letter; В файл init добавлены функции click_on_the_keyboard_button, find_letter_on_the_keyboard; В файл test добавлен тест test_yandex_search_button_keyboard

---
+++ LOCATORS.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class Locators:
+    search_field = (By.ID, "text")
+    search_button = (By.CLASS_NAME, "search3__button.mini-suggest__button")
+    clear_button = (By.CLASS_NAME, "search3__svg_clear")
+    element_nav_bar1 = (By.LINK_TEXT, "Картинки")
+    keyboard_button = (By.CLASS_NAME, "search3__svg_keyboard")
+    keyboard_letter = (By.CLASS_NAME, "keyboard__key-m")
