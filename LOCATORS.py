0ab02460fed3c8b22cb1f63250a7e4d69b6dc05a
AngelikaMilchenko <angelika.mila55@gmail.com>
2022-10-02 14:00:01 +0500

    В файл LOCATORS добавлены локаторы keyboard_button, keyboard_letter;

--- LOCATORS.py
+++ LOCATORS.py
@@ -5,5 +5,6 @@ class Locators:
     search_button = (By.CLASS_NAME, "search3__button.mini-suggest__button")
     clear_button = (By.CLASS_NAME, "search3__svg_clear")
     element_nav_bar1 = (By.LINK_TEXT, "Картинки")
+
     keyboard_button = (By.CLASS_NAME, "search3__svg_keyboard")
     keyboard_letter = (By.CLASS_NAME, "keyboard__key-m")
